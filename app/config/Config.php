9a731461b85215a20decc73b210ee81758bf5d62
Lucas <Apoena>
2019-08-02 10:47:26 -0300

    PHP-MVC-CRUD::Arquivo para centralizar as configurações

---
+++ app/config/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('BASE_DIR', "/bleez/PHP-CRUD-MVC");
+define('ROOT_PATH', $_SERVER['CONTEXT_DOCUMENT_ROOT'] . BASE_DIR);
+
+# Configurações do banco de dados
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'db_loja');
+
